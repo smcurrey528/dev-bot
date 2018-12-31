09323a86f28816ce9998ef3be9a4cfefea2bfdce
Sarah Currey <curreysarah28@gmail.com>
2018-12-31 13:56:49 -0600

    changed error message

--- index.js
+++ index.js
@@ -119,16 +119,13 @@ controller.hears(
     }
 );
 
-controller.hears('tacos','direct_mention,direct_message', function(bot, message) {
+controller.hears('error','direct_mention, mention, direct_message', function(bot, message) {
 
   bot.startConversation(message, function(err, convo) {
-    convo.say('Oh boy, taco time!');
-    convo.ask('What type of taco do you want?', function(answer, convo) {
-      var taco_type = answer.text;
-      // do something with this answer!
-      // storeTacoType(convo.context.user, taco_type);
-      convo.say('YUMMMM!!!'); // add another reply
-      convo.next(); // continue with conversation
+    convo.say('Oh boy, dev tools time!');
+    convo.ask('Why did the functional component feel lost?', function(answer, convo) {
+      var error_type = answer.text;
+      convo.say('Because it didn’t know what state it was in!');
     });
   });
 });
